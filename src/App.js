70bf41f64f40092fb91a52b06b924f06194de907
Kaito0208 <minhtranplus.0208@gmail.com>
2021-05-22 17:34:42 +0700

    feat(no-jira): update structure page

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+const Container = styled.div`
+background: red;
+color: #fff;
+font-size: 2rem;
+`
+
+const App = () => {
+    return (
+        <Container>
+            <h1>Minh</h1>
+        </Container>
+    )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-(function helloWorld() {
-    console.log('Hello World');
-}());
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+import './index.css';
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.querySelector("#root")
+);
